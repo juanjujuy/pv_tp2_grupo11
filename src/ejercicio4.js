e448bd697d36aba45cebebc3aa59797a90c18cd2
unknown <deliacusipuma80@gmail.com>
2025-04-10 19:27:23 -0300

    ejercicio 4 de tp2 realizado por Delia Cusipuma

---
+++ src/ejercicio4.js
@@ -0,0 +1,7 @@
+let lado=5;
+
+let area = 2 * lado;
+let perimetro = lado + lado + lado + lado;
+console.log("Lado del cuadrado:",lado);
+console.log("Area del cuadrado:",area);
+console.log("Perimetro del cuadrado:",perimetro);

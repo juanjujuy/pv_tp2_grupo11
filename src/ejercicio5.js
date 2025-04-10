c49e7bb50327a4ad988c39b515738d06f6920aeb
unknown <deliacusipuma80@gmail.com>
2025-04-10 20:08:07 -0300

    ejercicio 5  Delia Cusipuma

--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -1,3 +1,6 @@
+/*Declarar dos variables denominadas horas, minutos, asignar valores a cada una y 
+realizar la conversión para expresar esas horas y minutos en segundos. Mostrar por consola 
+las horas y minutos, luego el resultado de la conversión en segundos.*/
 let horas = 2;
 let minutos = 30;
 let totalsegundos = (horas * 3600) + (minutos * 60);

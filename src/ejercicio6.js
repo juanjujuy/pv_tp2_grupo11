c0977396f3f9f7059141eb005bf2aeaeebe00fd1
unknown <deliacusipuma80@gmail.com>
2025-04-10 20:09:40 -0300

    ejercicio 6  Delia Cusipuma

--- src/ejercicio6.js
+++ src/ejercicio6.js
@@ -1,3 +1,5 @@
+/*Crear un array denominado edades con al menos 8 edades distintas.
+Recorrer el array y obtener el promedio de las edades del array.*/
 let edades = [20, 25, 30, 22, 28, 27, 19, 24];
 
 let suma = 0;

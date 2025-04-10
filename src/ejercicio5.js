2453745c2338969703271c3187501cad66c5503d
unknown <deliacusipuma80@gmail.com>
2025-04-10 19:54:46 -0300

    ejercicio 5  Delia Cusipuma

---
+++ src/ejercicio5.js
@@ -0,0 +1,6 @@
+let horas = 2;
+let minutos = 30;
+let totalsegundos = (horas * 3600) + (minutos * 60);
+console.log("Horas:", horas);
+console.log("Minutos:", minutos);
+console.log("Total en segundos:", totalsegundos);

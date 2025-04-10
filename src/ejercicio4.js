d77f9ba850ab373a2ae3cf13d206098ea6e7b39e
unknown <deliacusipuma80@gmail.com>
2025-04-10 20:06:34 -0300

    ejercicio 4  Delia Cusipuma

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -1,4 +1,9 @@
-let lado=5;
+/*Declarar una variable y almacenar en ella un valor numérico que representa el lado de 
+un cuadrado. Calcular el área y el perímetro del cuadrado y mostrar los resultados por 
+consola.
+Area = 2 * lado
+Perímetro = lado + lado + lado + lado
+let lado=5;*/
 
 let area = 2 * lado;
 let perimetro = lado + lado + lado + lado;

10ec4ace99e3d580e84635c3bdf60395532081af
unknown <deliacusipuma80@gmail.com>
2025-04-10 21:34:15 -0300

    ejercicio 5  Delia Cusipuma

--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -1,9 +1,9 @@
 /*Declarar dos variables denominadas horas, minutos, asignar valores a cada una y 
 realizar la conversión para expresar esas horas y minutos en segundos. Mostrar por consola 
 las horas y minutos, luego el resultado de la conversión en segundos.*/
-let horas = 2;
+let Horas = 2;
 let minutos = 30;
-let totalsegundos = (horas * 3600) + (minutos * 60);
-console.log("Horas:", horas);
+let totalsegundos = (Horas * 3600) + (minutos * 60);
+console.log("Horas:", Horas);
 console.log("Minutos:", minutos);
 console.log("Total en segundos:", totalsegundos);

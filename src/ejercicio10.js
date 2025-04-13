54a5119ffbdf21c0e74198e10c10a208df1d7088
mati465 <146906100+mati465@users.noreply.github.com>
2025-04-13 17:28:28 -0300

    Add files via upload

---
+++ src/ejercicio10.js
@@ -0,0 +1,7 @@
+const num1 = parseInt(prompt("Ingrese el primer número entero:"));
+const num2 = parseInt(prompt("Ingrese el segundo número entero:"));
+const num3 = parseInt(prompt("Ingrese el tercer número entero:"));
+
+const promedio = (num1 + num2 + num3) / 3;
+
+alert(`El promedio de los tres números es: ${promedio}`);

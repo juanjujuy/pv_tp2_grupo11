073605572f356544a5eb5982b9a4393a44ddad82
unknown <deliacusipuma80@gmail.com>
2025-04-16 17:38:48 -0300

    ejercicio 4  Delia Cusipuma

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -2,10 +2,10 @@
 un cuadrado. Calcular el área y el perímetro del cuadrado y mostrar los resultados por 
 consola.
 Area = 2 * lado
-Perímetro = lado + lado + lado + lado
-let lado=5;*/
+Perímetro = lado + lado + lado + lado*/
 
-let Area = 2 * lado;
+let lado=5;
+let Area = lado * lado;
 let perimetro = lado + lado + lado + lado;
 console.log("Lado del cuadrado:",lado);
 console.log("Area del cuadrado:",Area);
